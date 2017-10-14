06daa5d403bf67176dcd992e6ea5feeafd58bdc7
Luxor002 <stefano.belli4@studio.unibo.it>
2017-10-14 18:13:04 +0200

    Aggiunto altro codice per il frontend
    -Definiti alcuni elementi fondamentali per l'app (Header, Toolbar e Main) e costruiti i rispettivi components.
    -Pulizia di codice inutile.

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
