beb66eddbbee50dc4155e975d2c202d61235a109
Eugenio Severi <eugies93@gmail.com>
2018-01-13 13:15:16 +0100

    Implementazione Cron e TLS nel backend.
    TODO: modificare il controller per esporre la funzione "performScraping()".

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const Hapi = require('hapi');
 const cron = require('cron');
 const Inert = require('inert');
-//carica tutte le routes di routes.js
-var controller = require('./backend/controller');
+const controller = require('./backend/controller');
 const config = require('./backend/config');
-
 const Path = require('path');
+const fs = require('fs');
+
+try { // Impostazioni di TLS (opzionali)
+  config.serverConnection.tls = {
+    key: fs.readFileSync(config.security.tlsPrivateKeyPath),
+    cert: fs.readFileSync(config.security.tlsCertificatePath)
+  }
+  console.log("INFO: Certificato e chiave privata caricati correttamente.");
+} catch (ex) {
+  console.log("INFO: File della chiave privata e/o del certificato non trovato/i.")
+}
+
 const server = new Hapi.Server(config.serverConnection);
 server.connections = {
   routes: {
@@ -19,7 +29,18 @@ server.route(controller);
 async function startServer(){
   await server.register(Inert);
   await server.start();
+  try { // Cron
+    cron.job(config.scraper.cronFrequency, function() {
+      console.log("INFO: Performing scraping with cron.");
+      controller.performScraping();
+    }).start();
+  } catch (ex) {
+    console.log("WARNING: Errore di sintassi nella specifica cron.");
+  }
+  if(config.scraper.forceScrapingOnStartup) { // Forced scraping
+    console.log("INFO: Performing forced scraping.");
+    controller.performScraping();
+  }
 }
 startServer();
 console.log('Server avviato su:', server.info.uri);
-
